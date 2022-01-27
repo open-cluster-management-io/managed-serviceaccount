794180d46410a3cbba53ad2e63191170604822ea
yue9944882 <291271447@qq.com>
2022-01-27 23:35:37 +0800

    e2e: follow-up
    Signed-off-by: yue9944882 <291271447@qq.com>

---
+++ e2e/token/e2e_test.go
@@ -0,0 +1,24 @@
+package token
+
+import (
+	"os"
+	"testing"
+
+	"github.com/onsi/ginkgo"
+	"github.com/onsi/gomega"
+	"open-cluster-management.io/managed-serviceaccount/e2e/framework"
+)
+
+func TestMain(m *testing.M) {
+	gomega.RegisterFailHandler(ginkgo.Fail)
+	framework.ParseFlags()
+	os.Exit(m.Run())
+}
+
+func RunE2ETests(t *testing.T) {
+	ginkgo.RunSpecs(t, "ManagedServiceAccount e2e suite -- token tests")
+}
+
+func TestE2E(t *testing.T) {
+	RunE2ETests(t)
+}
