b3c5e6cdd3b7d394924a005fb034170cacc921f6
Hao Liu <44379968+TheRealHaoLiu@users.noreply.github.com>
2022-05-05 20:06:15 -0400

    add agent-image-pull-secret param to manager (#40)
    if the agent image is not publicly accessible by the managed cluster image pull secret will need to be added to the managed cluster by hub addon manager via manifestwork
    
    this PR add a new parameter and env var to the addon manager to allow user to optionally specify a reference to an image pull secret on the hub.
    
    when this parameter is specified the content of the secret will be copied to the agent install namespace on the managed cluster and be use by agent deployment to pull the image
    
    Signed-off-by: Hao Liu <haoli@redhat.com>

--- e2e/framework/scheme.go
+++ e2e/framework/scheme.go
@@ -2,11 +2,11 @@ package framework
 
 import (
 	"k8s.io/apimachinery/pkg/runtime"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	k8sscheme "k8s.io/client-go/kubernetes/scheme"
 	addonv1alpha1 "open-cluster-management.io/api/addon/v1alpha1"
 	clusterv1 "open-cluster-management.io/api/cluster/v1"
 	authenticationv1alpha1 "open-cluster-management.io/managed-serviceaccount/api/v1alpha1"
-	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 )
 
 var scheme = runtime.NewScheme()
