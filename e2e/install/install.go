dc3b0c11fe00b0ceecb3d00aff9eb281071f52ff
Wei Liu <liuweixa@redhat.com>
2022-11-09 08:11:40 +0000

    support addon deployment config
    Signed-off-by: Wei Liu <liuweixa@redhat.com>

--- e2e/install/install.go
+++ e2e/install/install.go
@@ -2,12 +2,17 @@ package install
 
 import (
 	"context"
+	"fmt"
 	"time"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/equality"
 	"k8s.io/apimachinery/pkg/api/meta"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 	addonv1alpha1 "open-cluster-management.io/api/addon/v1alpha1"
 	"open-cluster-management.io/managed-serviceaccount/e2e/framework"
@@ -29,5 +34,116 @@ var _ = Describe("Addon Installation Test",
 				Expect(err).NotTo(HaveOccurred())
 				return meta.IsStatusConditionTrue(addon.Status.Conditions, addonv1alpha1.ManagedClusterAddOnConditionAvailable), nil
 			}).WithTimeout(time.Minute).Should(BeTrue())
+		})
+
+		It("Addon should can be configured with AddOnDeployMentConfig", func() {
+			deployConfigName := "deploy-config"
+			nodeSelector := map[string]string{"kubernetes.io/os": "linux"}
+			tolerations := []corev1.Toleration{{Key: "node-role.kubernetes.io/infra", Operator: corev1.TolerationOpExists, Effect: corev1.TaintEffectNoSchedule}}
+
+			c := f.HubRuntimeClient()
+			By("Prepare a AddOnDeployMentConfig for managed-serviceaccount addon")
+			Eventually(func() error {
+				return c.Create(context.TODO(), &addonv1alpha1.AddOnDeploymentConfig{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      deployConfigName,
+						Namespace: f.TestClusterName(),
+					},
+					Spec: addonv1alpha1.AddOnDeploymentConfigSpec{
+						NodePlacement: &addonv1alpha1.NodePlacement{
+							NodeSelector: nodeSelector,
+							Tolerations:  tolerations,
+						},
+					},
+				})
+			}).WithTimeout(time.Minute).ShouldNot(HaveOccurred())
+
+			By("Add the config to managed-serviceaccount addon")
+			Eventually(func() error {
+				addon := &addonv1alpha1.ManagedClusterAddOn{}
+				if err := c.Get(context.TODO(), types.NamespacedName{
+					Namespace: f.TestClusterName(),
+					Name:      "managed-serviceaccount",
+				}, addon); err != nil {
+					return err
+				}
+
+				addon.Spec.Configs = []addonv1alpha1.AddOnConfig{
+					{
+						ConfigGroupResource: addonv1alpha1.ConfigGroupResource{
+							Group:    "addon.open-cluster-management.io",
+							Resource: "addondeploymentconfigs",
+						},
+						ConfigReferent: addonv1alpha1.ConfigReferent{
+							Namespace: f.TestClusterName(),
+							Name:      deployConfigName,
+						},
+					},
+				}
+
+				return c.Update(context.TODO(), addon)
+			}).WithTimeout(time.Minute).ShouldNot(HaveOccurred())
+
+			By("Ensure the config is referenced")
+			Eventually(func() error {
+				addon := &addonv1alpha1.ManagedClusterAddOn{}
+				if err := c.Get(context.TODO(), types.NamespacedName{
+					Namespace: f.TestClusterName(),
+					Name:      "managed-serviceaccount",
+				}, addon); err != nil {
+					return err
+				}
+
+				if len(addon.Status.ConfigReferences) == 0 {
+					return fmt.Errorf("no config references in addon status")
+				}
+				if addon.Status.ConfigReferences[0].Name != deployConfigName {
+					return fmt.Errorf("unexpected config references %v", addon.Status.ConfigReferences)
+				}
+				return nil
+			}).WithTimeout(time.Minute).ShouldNot(HaveOccurred())
+
+			By("Ensure the cluster-proxy is configured")
+			Eventually(func() error {
+				deploy := &appsv1.Deployment{}
+				if err := c.Get(context.TODO(), types.NamespacedName{
+					Namespace: common.AddonAgentInstallNamespace,
+					Name:      "managed-serviceaccount-addon-agent",
+				}, deploy); err != nil {
+					return err
+				}
+
+				if deploy.Status.AvailableReplicas != *deploy.Spec.Replicas {
+					return fmt.Errorf("unexpected available replicas %v", deploy.Status)
+				}
+
+				if !equality.Semantic.DeepEqual(deploy.Spec.Template.Spec.NodeSelector, nodeSelector) {
+					return fmt.Errorf("unexpected nodeSeletcor %v", deploy.Spec.Template.Spec.NodeSelector)
+				}
+
+				if !equality.Semantic.DeepEqual(deploy.Spec.Template.Spec.Tolerations, tolerations) {
+					return fmt.Errorf("unexpected tolerations %v", deploy.Spec.Template.Spec.Tolerations)
+				}
+				return nil
+			}).WithTimeout(time.Minute).ShouldNot(HaveOccurred())
+
+			By("Ensure the managed-serviceaccountis available")
+			Eventually(func() error {
+				addon := &addonv1alpha1.ManagedClusterAddOn{}
+				if err := c.Get(context.TODO(), types.NamespacedName{
+					Namespace: f.TestClusterName(),
+					Name:      "managed-serviceaccount",
+				}, addon); err != nil {
+					return err
+				}
+
+				if !meta.IsStatusConditionTrue(
+					addon.Status.Conditions,
+					addonv1alpha1.ManagedClusterAddOnConditionAvailable) {
+					return fmt.Errorf("addon is unavailable")
+				}
+
+				return nil
+			}).WithTimeout(time.Minute).ShouldNot(HaveOccurred())
 		})
 	})
